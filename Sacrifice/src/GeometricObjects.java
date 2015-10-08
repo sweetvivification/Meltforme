68ba139d6e5c794847a41fdbacee2142953028f2
sweetvivification <michimatsch@web.de>
2015-10-08 08:30:19 +0200

    rekt

--- Sacrifice/src/GeometricObjects.java
+++ Sacrifice/src/GeometricObjects.java
@@ -1,63 +1,64 @@
+public class GeometricObject {
 
-public class GeometricObjects {
-public Vertex pos;
-double height;
-double width;
-
-public double getHeight(){return height;
-}
-public double getWidth(){return width;
-}
-public Vertex getPos(){return pos;
-}
-
-public GeometricObjects(double height, double width, Vertex pos){
-	this.pos=pos;
-	this.height=height;
-	this.width=width;
+	public Vertex pos;
+	public double height;
+	public double width;
 	
-	if(width<0){
-		width= -width;
-		pos.x =pos.x -width;
-	}
-	if(height<0){
-		height = -height;
-		pos.y =pos.y -height;
+	
+	public GeometricObject(double width, double height, Vertex pos){
+		this.pos=pos;
+		this.width=width;
+		this.height=height;
 		
-	}
-}
-public GeometricObjects(double hight, double width){
-	this(hight, width, new Vertex(0,0));
-}
-
-public GeometricObjects(double height){
-	this(height,height);
-}
-public GeometricObjects(){
-	this(10);
-}
-public GeometricObjects(Vertex pos){
-	this(0,0,pos);
-}
-public String toString(){
-	return "width="+width+"height="+height+"position="+pos;
+		if(this.width<0){
+			this.width = -this.width;
+			pos.x = pos.x -this.width;
+			}
+		if(this.height<0){
+			this.height = -this.height;
+			pos.y = pos.y -this.height;
+			}
 	
+	}
+	
+	public GeometricObject(double width, double height){
+		this(width, height, new Vertex(0,0));
+	}
+	
+	public GeometricObject(double width){this(width,width);}
+	
+	public GeometricObject(Vertex pos){this(0,0,pos);}
+	
+    public double getWidth(){return width;}
+	
+    public double getHeight(){return height;}
+    
+    public Vertex getPos(){return pos;}
+	
+    public String toString(){
+    	return "width="+width+", height="+height+", pos="+pos;
+    }
+    public double circumference(){return 2*(width+height);} 
+        
+    public double area(){return width*height;}
+    
+    public boolean contains(Vertex v){
+    	return v.x >=pos.x && v.x <=pos.x+width // ist x drin?
+    	&& v.y >=pos.y && v.y <=pos.y+height;  // ist y drin?
+    }
+    
+    public boolean isLargerThan(GeometricObject that){
+    	return this.area()>that.area();
+    }
+    
+    public void moveTo(Vertex pos){this.pos=pos;}
+    
+    public void moveTo(double x, double y){
+    	moveTo(new Vertex(x,y));
+    }
+    
+    public void move(Vertex v){moveTo(pos.add(v));
 }
-public double circumference(){
-	return 2*(width+height);
-	}//Umfang
-public double area(){
-	return height*width;
 }
-public boolean contains(Vertex v){
-	return v.x >= pos.x && v.x <=pos.x+width &&
-			v.y >=pos.y && v.y <=pos.y+height;
-}
-public boolean isLargerThan(GeometricObjects that)
-{return that.area() > this.area();
 
-}
-public void moveTo(Vertex pos){
-	this.pos=pos;
-}
-}
+
